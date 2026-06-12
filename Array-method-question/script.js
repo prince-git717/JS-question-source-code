1df8747b0da00519f5cb25fd9d463006b5b1742e
Prince raj <jaaprince999@gmail.com>
2026-06-12 15:11:50 +0530

    this is the array method question

---
+++ Array-method-question/script.js
@@ -0,0 +1,116 @@
+
+// questions on push() method
+// question number 1
+// const arr =[1,2,3];
+// const result = arr.push(4);
+// console.log(arr);
+// console.log(result)
+// question no 2
+// const arr1 =[1,2];
+// const arr2 =arr1;
+// arr2.push(3);
+// console.log(arr1)
+// question no 3
+
+// const arr =[10,20,30];
+// const result =arr.pop();
+// console.log(arr);
+// console.log(result)
+//  question no 4.
+// const arr =[1,2,3]
+// console.log(arr.pop());
+// console.log(arr.pop());
+// console.log(arr.pop());
+// console.log(arr);
+// question number 5
+
+// const arr =[1,2,3,4];
+// const result =arr.shift();
+// console.log(arr);
+// console.log(result)
+
+// question number 6
+// const arr =[3,4];
+// const result = arr.unshift(1,2);
+// console.log(arr);
+// console.log(result);
+// question number 7
+// const arr1= [1,2];
+// const arr2 =[3,4];
+// const result = arr1.concat(arr2);
+// console.log(result)
+// Q8 (Interview)
+// const arr1=[1,2];
+// const arr2 =arr1.concat([3]);
+// console.log(arr1===arr2);
+// question no 9.
+// const arr =[1,2,3,2,4];
+// console.log(arr.indexOf(2,2));
+// question number 10
+// const arr =[{id:1} , {id:2}];
+// console.log(arr.indexOf({id:1}));
+// question number 11
+// const arr =[1,2,3];
+// console.log(arr.includes(2));
+// console.log(arr.includes(5));
+// Q12 (Interview)
+// const arr =[{id:1}];
+// console.log(arr.includes({id:1}));
+// question number 13
+// const arr =[1,2,3];
+// const reversed =arr.reverse();
+// console.log(arr===reversed);
+// question number 14
+// const arr =[1,2,3];
+// const reversed = arr.reverse();
+// reversed.push(4);
+// console.log(arr);
+// question number 15
+// const arr = [100,25,3]
+// arr.sort();
+// console.log(arr);
+// answer should be like 100, 25,3 i guess what you gess guys 
+// const arr =[100,25,3];
+// arr.sort((a,b)=>a-b);
+// console.log(arr)
+// question number 17
+// const arr =[10,2,1];
+// arr.sort((a,b)=> b-a);
+// console.log(arr);
+//  question number 18.
+// const arr=[1,2,3,4,5];
+// const result =arr.slice(1,4);
+// console.log(arr);
+// console.log(result)
+// slice(start, stopBefore)
+// slice does not change the original array but splice changes the original array .
+// qustion number 19.
+// const arr = [1,2,3];
+// const copy = arr.slice();
+// copy.push(4);
+// console.log(arr);
+// console.log(copy);
+// question number 20
+
+// const arr =[1,2,3,4];
+// const result =arr.splice(1,2);
+// console.log(arr);
+// console.log(result)
+// Final Interview Question 🔥
+const arr1 =[1,2,3];
+const arr2 =arr1.reverse();
+// console.log(arr1);
+const arr3=arr1.concat([4]);
+// console.log(arr3)
+arr2.push(5); 
+console.log(arr1)
+// so it should add the 5 in the last i guess 
+// okay guys that's it for toudy ,, if possibel i will come live at night and try to solve and clear concept with you .. stay tuned if yu want these question to practice then you can simply visit my github account i will attach the link in the discription and that's it thank you all 
+
+
+
+
+
+
+
+
